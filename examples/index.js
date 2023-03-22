f20e075d16293f518af5d3f04b8422d562480689
Roman-Ojha <razzroman98@gmail.com>
2023-03-23 00:16:17 +0545

    feat: used & create example with the published npm package

---
+++ examples/index.js
@@ -0,0 +1,7 @@
+import { Math } from "roman-simple-math";
+
+const math = new Math();
+console.log(math.add(1, 2));
+console.log(math.subtract(1, 2));
+console.log(math.multiply(1, 2));
+console.log(math.division(1, 2));
--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,18 @@
             -> so because our entry point is index.js file which will get build in lib folder, so we will specify the entry point as 'lib/index.js' inside package.json
         -> we also have to specify where our types or declaration file exist so that other developers can get the intellisense for our package
             -> so we will specify the path of our declaration file inside 'types' property in package.json
+
+    *) Publishing the package to npm registry:
+      -> if you don't have an account on npm registry, then first you have to create an account on npm registry
+      -> first we have to login to npm registry using 'npm login' command
+      -> if you want to see you username, then you can use 'npm whoami' command
+      -> after that we have to publish our package using 'npm publish' command
+
+    *) Using the published package:
+      -> now we have successfully published our package now we will use this package in another project '../example'
+      -> 
+        -> npm i roman-simple-math
+
 */
 
 interface MathType {
@@ -24,7 +36,7 @@ interface MathType {
   division(a: number, b: number): number;
 }
 
-export default class Math implements MathType {
+export class Math implements MathType {
   add(a: number, b: number): number {
     return a + b;
   }
